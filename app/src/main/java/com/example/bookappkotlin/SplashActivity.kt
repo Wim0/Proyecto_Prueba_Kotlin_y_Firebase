c96f872cee0bda0981970cb573605df25e5161c5
danielreva <83436148+danielreva@users.noreply.github.com>
2023-02-01 16:02:36 -0300

    Progreso Día 3

--- app/src/main/java/com/example/bookappkotlin/SplashActivity.kt
+++ app/src/main/java/com/example/bookappkotlin/SplashActivity.kt
@@ -1,11 +1,19 @@
 package com.example.bookappkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.coroutines.delay
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //Inicia la MainActivity después de 2 segundos
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+        }, 2000) //Indica un delay de 2 segundos
     }
 }
